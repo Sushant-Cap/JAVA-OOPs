bd21be29d896f46a603ddfff3ba347183308112a
Sushant-cap <sushantkamble9284@gmail.com>
2023-01-31 19:33:09 +0530

    this is my 1st commit

---
+++ java_fundamentals/src/java_fundamentals/Loop.java
@@ -0,0 +1,15 @@
+//Loops in Java (Simple for Loop)
+
+package java_fundamentals;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+   for(int i=1;i<=100;i++){  
+       System.out.println(i);  
+
+	}
+
+}
+}
---
+++ java_fundamentals/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sbalukam
+ *
+ */
+module java_fundamentals {
+}
