33ddd45ad2e6f58f5f27ea16d898862340b09024
Sushant-cap <sushantkamble9284@gmail.com>
2023-02-01 20:14:22 +0530

    this is my 1st commit

---
+++ java_fundamentals/src/java_fundamentals/For_Each.java
@@ -0,0 +1,33 @@
+//The for-each loop is used to traverse array or collection in Java.
+//It is easier to use than simple for loop because we don't 
+//need to increment value and use subscript notation.
+
+//It works on the basis of elements and not the index. 
+//It returns element one by one in the defined variable.
+
+
+
+//Java For-each loop example which prints the  
+//elements of the array 
+
+//package java_fundamentals;
+//public class For_Each {  
+//public static void main(String[] args) {  
+//  //Declaring an array  
+//  int arr[]={12,23,44,56,78};  
+//  //Printing array using for-each loop  
+//  for(int i:arr){  
+//      System.out.println(i);  
+//  }  
+//}  
+
+package java_fundamentals;
+public class For_Each{
+	public static void main (String[] args) {
+		int arr[]= {1,2,3,4,5,66,77};
+		for (int i:arr) {
+			System.out.println(i);
+		}
+	}
+	
+}
