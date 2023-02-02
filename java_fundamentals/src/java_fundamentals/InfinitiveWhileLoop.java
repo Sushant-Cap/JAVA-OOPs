1ee3d7b55b42eff5dd1a51da84e6e277f9575cf9
Sushant-cap <sushantkamble9284@gmail.com>
2023-02-02 20:13:53 +0530

    loops are done oops are start

---
+++ java_fundamentals/src/java_fundamentals/InfinitiveWhileLoop.java
@@ -0,0 +1,12 @@
+//infinite while loop When condition is always true 
+
+package java_fundamentals;
+
+public class InfinitiveWhileLoop {    
+public static void main(String[] args) {   
+ // setting the infinite while loop by passing true to the condition  
+    while(true){    
+        System.out.println("infinitive while loop");    
+    }    
+}    
+}    
