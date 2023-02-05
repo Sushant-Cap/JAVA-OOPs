9a438f0c54ade781a105f2edb3aa5b938e94b8f9
Sushant-cap <sushantkamble9284@gmail.com>
2023-02-05 14:32:42 +0530

    1st commit of oops

---
+++ OOPs/src/oops/Bird.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Bird extends Animal{
+
+	public Bird(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+
+	public void fly() { 
+		System.out.println("Flying....");
+		
+	}
+
+}
---
+++ java_fundamentals/src/java_fundamentals/OOPs/OOPs.java
@@ -0,0 +1,5 @@
+package java_fundamentals.OOPs;
+
+public class OOPs {
+
+}
